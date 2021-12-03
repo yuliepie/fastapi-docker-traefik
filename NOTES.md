e87496ae17a6204fe285c45ebe0d7c218837648a
Yulie Jung <yuliekorea@gmail.com>
2021-12-03 22:46:00 +0900

    Setup Postgres and User model

--- NOTES.md
+++ NOTES.md
@@ -9,3 +9,29 @@ Check docker logs:
 ```bash
 docker-compose logs -f
 ```
+
+Check db
+- List dbs
+- Connect to db
+- check tables
+- quit
+```bash
+$ docker-compose exec db psql --username=fastapi_traefik --dbname=fastapi_traefik
+
+fastapi_traefik=# \l
+fastapi_traefik=# \dt
+
+fastapi_traefik=# \q
+```
+
+Check db volume created
+```bash
+$ docker volume inspect fastapi-docker-traefik_postgres_data
+```
+
+## Ormar
+- An ORM that is built on top of SQLAlchemy and Pydantic
+- Ormar models serve as both Pydantic & database models
+- Uses SQLAlchemy for creating tables & queries
+- Uses databases for async execution of queries
+- pydantic for model validation
--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 
+from app.db import database, User # This creates all tables on import
+
 app = FastAPI(title="FastAPI, Docker and Traefik!")
 
+#=====================
+# Database connection
+#=====================
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+    # create a dummy user
+    await User.objects.get_or_create(email="test@test.com")
 
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
+
+
+# Get all users
 @app.get("/")
-def read_root():
-    return {"hello": "world"}
+async def read_root():
+    return await User.objects.all()
